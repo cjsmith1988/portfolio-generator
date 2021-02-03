a4ae9701408808027e7a367c860411ee30859c76
Curtis Smith <cj1988333@hotmail.com>
2021-02-02 23:59:12 -0500

    added html module

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+const profileDataArgs = process.argv.slice(2);
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i+= 1) {
-        console.log(profileDataArr[i]);
-    }
-    console.log('================');
-    profileDataArr.forEach((profileItem) => console.log(profileItem));        
-    
-};
-    
+const [name, github] = profileDataArgs;
 
-printProfileData(profileDataArgs);
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
