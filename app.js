e562a5ca3974292790ba23aadae5c334ad0bd8b4
Curtis Smith <cj1988333@hotmail.com>
2021-02-07 00:01:03 -0500

    finished project

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 
- const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
  const generatePage = require('./src/page-template.js');
 
 const promptUser = () => {
@@ -131,7 +131,6 @@ const promptProject = portfolioData => {
     });
     
 };
-
 const mockData = {
     name: 'Lernantino',
     github: 'lernantino',
@@ -178,15 +177,20 @@ const mockData = {
     ]
   };
 promptUser()
-//.then(promptProject)
-.then(portfolioData => {
-
-
-// const pageHTML = generatePage(portfolioData);
-   const pageHTML = generatePage(mockData);
-
-fs.writeFile('index.html', pageHTML, err => {
-    if (err) throw new Error(err);
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
-});
+    .then(promptProject)
+    .then(portfolioData => {
+        return generatePage(portfolioData);
+    })
+    .then(pageHTML => {
+      return writeFile(pageHTML);
+    })
+    .then(writeFileResponse => {
+      console.log(writeFileResponse);
+      return copyFile();
+    })
+    .then(copyFileResponse => {
+      console.log(copyFileResponse);
+    })
+    .catch(err => {
+      console.log(err);
+    });
